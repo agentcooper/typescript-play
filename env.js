6252ac89aa82b8ac176b39af02fffec89407107d
Artem Tyurin <artem.tyurin@uber.com>
2018-06-08 12:29:33 +0200

    Updates

--- env.js
+++ env.js
@@ -1,1 +1,1 @@
-window.localTSVersion = { '2.8.3': { monaco: '0.12.0', lib: '/monaco-typescript/2.8.3' } }
+window.localTSVersion = { '2.9.1': { monaco: '0.13.1', lib: '/monaco-typescript/2.9.1' } }
