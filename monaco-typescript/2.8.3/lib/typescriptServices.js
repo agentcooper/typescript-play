a64a67c2a4b969e6209cbfd2904c25fcbb52c0cd
Artem Tyurin <artem.tyurin@uber.com>
2018-06-08 11:55:46 +0200

    Updates

--- monaco-typescript/2.8.3/lib/typescriptServices.js
+++ monaco-typescript/2.8.3/lib/typescriptServices.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-typescript version: 3.1.0(64641eebb2d0dca62499cc1b822970ea72efbfce)
+ * monaco-typescript version: 3.1.0(03655e6da98b1bf61147a5fa3e5ff6fbf19aa00d)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-typescript/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
